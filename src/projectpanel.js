51a7e422619b1e0fafc2a32cb412c1254bd54765
quanbenn <77028868+quanbenn@users.noreply.github.com>
2023-03-30 18:33:27 -0400

    React calc

--- src/projectpanel.js
+++ src/projectpanel.js
@@ -30,17 +30,13 @@ export default function ProjectPanel(){
             changeClasses(["the-image z-10"]);
             changeImage("/images/Eric.png");
         }
-        if(n === 7){
-            changeClasses(["the-image z-10"]);
-            changeImage("/images/hundreds.jpeg");
-        }
-        if(n === 8){
-            changeClasses(["the-image z-10"]);
-            changeImage("/images/hundreds.jpeg");
-        }
         if(n === 9){
             changeClasses(["the-image z-10"]);
             changeImage("/images/git3.png");
+        }
+        if(n === 10){
+            changeClasses(["the-image z-10"]);
+            changeImage("/images/upwork2.png");
         }
     }
 
@@ -57,44 +53,50 @@ export default function ProjectPanel(){
             <ul className="scrolldown-panel no-scrollbar">
                 <ul onMouseOver={() => testHover(1)} onMouseLeave={() => testMouseOut()}>
                     <li>
-                        <h1 className="project-name">Brentwood Rotary</h1>
+                        <h1 className="project-name"><a href="https://rotaryclubbrentwood.org/">Brentwood Rotary</a></h1>
                         <p className="project-text">Website</p>
                     </li>
                 </ul>
                 <ul onMouseOver={() => testHover(2)} onMouseLeave={() => testMouseOut()}>
                     <li>
-                        <h1 className="project-name">Close to Ten</h1>
+                        <h1 className="project-name"><a href="https://quanahb.github.io/MenuTest/index.html">Close to Ten</a></h1>
                         <p className="project-text">Game</p>
                     </li>
                 </ul>
                 <ul onMouseOver={() => testHover(3)} onMouseLeave={() => testMouseOut()}>
                     <li>
-                        <h1 className="project-name">My Calulator</h1>
+                        <h1 className="project-name"><a href="https://roaring-seahorse-774bc7.netlify.app/">My Calulator</a></h1>
                         <p className="project-text">React App</p>
                     </li>
                 </ul>
                 <ul onMouseOver={() => testHover(4)} onMouseLeave={() => testMouseOut()}>
                     <li>
-                        <h1 className="project-name">Anais</h1>
+                        <h1 className="project-name"><a href="https://anaisparmakian.com/">Anais</a></h1>
                         <p className="project-text">Personal Portfolio</p>
                     </li>
                 </ul>
                 <ul onMouseOver={() => testHover(5)} onMouseLeave={() => testMouseOut()}>
                     <li>
-                        <h1 className="project-name">Thalyta</h1>
+                        <h1 className="project-name"><a href="https://thalytaswansonphotography.com/">Thalyta</a></h1>
                         <p className="project-text">Photography Portfolio</p>
                     </li>
                 </ul>
                 <ul onMouseOver={() => testHover(6)} onMouseLeave={() => testMouseOut()}>
                     <li>
-                        <h1 className="project-name">Eric Mackey</h1>
+                        <h1 className="project-name"><a href="https://www.ericmackey.com/">Eric Mackey</a></h1>
                         <p className="project-text">Art Portfolio</p>
                     </li>
                 </ul>
                 <ul onMouseOver={() => testHover(9)} onMouseLeave={() => testMouseOut()}>
                     <li>
-                        <h1 className="project-name">Github</h1>
+                        <h1 className="project-name"><a href="https://github.com/QuanahB">Github</a></h1>
                         <p className="project-text">Open Source</p>
+                    </li>
+                </ul>
+                <ul onMouseOver={() => testHover(10)} onMouseLeave={() => testMouseOut()}>
+                    <li>
+                        <h1 className="project-name"><a href="https://www.upwork.com/freelancers/~015e31f8128fd84074">Upwork</a></h1>
+                        <p className="project-text">Freelance Portfolio</p>
                     </li>
                 </ul>
             </ul>
